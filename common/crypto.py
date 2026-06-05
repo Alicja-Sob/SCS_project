a971797edf722842f6a79aa1fcf585acbe4852a6
milooox <milooox1337@gmail.com>
2026-06-05 13:35:08 +0200

    implement RSA key pair generation and certificate handling

---
+++ common/crypto.py
@@ -0,0 +1,55 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from dateutil.relativedelta import *
+import datetime
+import hashlib
+import os
+def generate_RSA_key_pair():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=4096)
+    public_key = private_key.public_key()
+
+    return public_key, private_key
+
+def serialize_public_key(public_key):
+    return public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+def serialize_private_key(private_key):
+    return private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+def deserialize_public_key(serialized_public_key):
+    return serialization.load_pem_public_key(serialized_public_key)
+
+def deserialize_private_key(serialized_private_key):
+    return serialization.load_pem_private_key(serialized_private_key, password=None)
+
+def generate_random_id():
+    return hashlib.sha256(os.urandom(32)).hexdigest()
+
+def generate_certificate(subject_id, subject_public_key, issuer_id, issuer_private_key):
+    builder = x509.CertificateBuilder()
+    
+    builder = builder.subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, subject_id)]))
+    builder = builder.issuer_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, issuer_id)]))
+    builder = builder.public_key(subject_public_key)
+    builder = builder.serial_number(x509.random_serial_number())
+    builder = builder.not_valid_before(datetime.datetime.now())
+    builder = builder.not_valid_after(datetime.datetime.now() + datetime.timedelta(weeks=4))
+
+    return builder.sign(issuer_private_key, hashes.SHA256())
+    
+
+def serialize_certificate(cert):
+    return cert.public_bytes(serialization.Encoding.PEM)
+
+def deserialize_certificate(serialized_cert):
+    return x509.load_pem_x509_certificate(serialized_cert)
