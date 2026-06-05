bd9bdb1472ddbf3dbafb4dfd919653df7cf40817
milooox <milooox1337@gmail.com>
2026-06-06 00:14:37 +0200

    added encrypted server registration and certificate exchange

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
 import logging
 import time
 import socket
-
+import crypto
+import json
+import base64
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -9,6 +11,13 @@ logging.basicConfig(
 logger = logging.getLogger("Server_App") 
 
 def main():
+
+    logger.info("generowanie id oraz kluczy przez serwer")
+    server_id = crypto.generate_random_id()
+    server_public_key, server_private_key = crypto.generate_RSA_key_pair()
+    logger.info(f"server id: {server_id}")
+
+
     logger.info("server działa")
     
     host = 'ttp'
@@ -19,19 +28,40 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((host, port))
-            logger.info("server : connected")
-            
-            s.sendall(b"ping dla ttp od serwera")
-            
-            data = s.recv(1024)
-            logger.info(f"wiadomosc : {data.decode('utf-8')}")
-            
+            data = s.recv(4096)
+            if data:
+                data = json.loads(data.decode('utf-8'))
+                ttp_id = data["id"]
+                ttp_public_key = crypto.deserialize_public_key(data["public_key"].encode('utf-8'))
+                logger.info(f"otrzymano dane od ttp: {data}")
+
+                encrypted_server_id = crypto.encrypt_data(ttp_public_key, server_id.encode('utf-8'))
+                encrypted_server_id_str = base64.b64encode(encrypted_server_id).decode('utf-8')
+                server_public_key_str = crypto.serialize_public_key(server_public_key).decode('utf-8')
+                payload = {
+                    "encrypted_server_id": encrypted_server_id_str,
+                    "server_public_key": server_public_key_str
+                }
+                s.sendall(json.dumps(payload).encode('utf-8'))
+                logger.info(f"wysłano zaszyfrowany id serwera oraz klucz publiczny do ttp: {payload}")
+                cert_response = s.recv(4096)
+                if cert_response:
+                    logger.info(f"otrzymano certyfikat od ttp: {cert_response.decode('utf-8')}")
+                    
+            # logger.info("server : connected")
+            # payload = {
+            #     "id": server_id,
+            #     "public_key": crypto.serialize_public_key(server_public_key)
+            # }
+            # s.sendall(json.dumps(payload).encode('utf-8'))
+
         except Exception as e:
             logger.error(f"error: {e}")
             
     try:
         while True:
             time.sleep(60)
+
     except KeyboardInterrupt:
         logger.info("shutdown")
 
--- ttp/main.py
+++ ttp/main.py
@@ -1,6 +1,8 @@
+import json
 import logging
 import socket
-
+import crypto 
+import base64
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -8,6 +10,16 @@ logging.basicConfig(
 logger = logging.getLogger("TTP_App") 
 
 def main():
+    logger.info("generowanie certyfikatu przez ttp")
+    ttp_id = crypto.generate_random_id()
+    
+    logger.info(f"ttp id: {ttp_id}")
+
+    ttp_public_key, ttp_private_key = crypto.generate_RSA_key_pair()
+    ttp_cert = crypto.generate_certificate(ttp_id, ttp_public_key, ttp_id, ttp_private_key)
+    
+    logger.info(f"certyfikat ttp wygenerowany: {ttp_cert}")
+
     logger.info("ttp dziala")
     
     host = '0.0.0.0'
@@ -20,12 +32,34 @@ def main():
         
         while True:
             conn, addr = s.accept()
+            payload = {
+                "id": ttp_id,
+                "public_key": crypto.serialize_public_key(ttp_public_key).decode('utf-8')
+            }
+            
             with conn:
+            
                 logger.info(f"polaczono : {addr}")
-                data = conn.recv(1024)
-                if data:
-                    logger.info(f"wiadomosc : {data.decode('utf-8')}")
-                    conn.sendall(b"ping od ttp dla serwera ")
+                conn.sendall(json.dumps(payload).encode('utf-8'))
+
+                data = conn.recv(4096)
+                if data: 
+                    data = json.loads(data.decode('utf-8'))
+                    encrypted_server_id = base64.b64decode(data["encrypted_server_id"])
+                    server_public_key = crypto.deserialize_public_key(data["server_public_key"].encode('utf-8'))
+                    server_id = crypto.decrypt_data(ttp_private_key, encrypted_server_id).decode('utf-8')
+                    logger.info(f"serwer id: {server_id}")
+
+                    server_cert = crypto.generate_certificate(server_id, server_public_key, ttp_id, ttp_private_key)
+
+                    serialized_cert = crypto.serialize_certificate(server_cert).decode('utf-8')
+                    conn.sendall(serialized_cert.encode('utf-8'))
+                    logger.info(f"wysłano certyfikat do serwera: {serialized_cert}")
+
+                # data = conn.recv(1024)
+                # if data:
+                #     logger.info(f"wiadomosc : {data.decode('utf-8')}")
+                #     conn.sendall(b"ping od ttp dla serwera ")
 
 if __name__ == "__main__":
     main()
